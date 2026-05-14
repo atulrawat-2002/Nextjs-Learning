ba291e2542f92867dc96cdefbae98b41064a1210
Atul <dr6217811@gmail.com>
2026-05-14 16:01:17 +0530

    Implementing different layout for root and authentication routes:

---
+++ my-app/app/(auth)/layout.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react"
+
+const AuthLayout = ({ children }: {
+    children: ReactNode
+}) => {
+  return (
+     <main>
+        {
+            children
+        }
+     </main>
+  )
+}
+
+export default AuthLayout
--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/navigation/navbar";
 import ThemeProvider from "@/context/Theme";
 
 const geistSans = Geist({
@@ -32,7 +31,6 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange >
-          <Navbar />
           {children}
         </ThemeProvider>
       </body>
