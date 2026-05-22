a79698368b2826c3c982a97a40ec2c9fa5c26b8c
Atul <dr6217811@gmail.com>
2026-05-22 08:34:15 +0530

    Authentication error in github auth]

--- my-app/components/forms/SocialAuthForm.tsx
+++ my-app/components/forms/SocialAuthForm.tsx
@@ -1,12 +1,36 @@
+'use client'
+
 import Image from "next/image"
 import { Button } from "../ui/button"
+import { toast } from "sonner"
+import { signIn } from "next-auth/react"
+import ROUTES from "@/constants/routes"
 
 const SocialAuthForm = () => {
     const buttonClass = "background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 rounded-2 px-4 py-3.5"
+
+    const handleSignInFunction = async (provider: "github" | "google") => {
+        try {
+            await signIn(provider, {
+                callbackUrl: ROUTES.SIGN_IN,
+                redirect: false
+            });
+        } catch (error) {
+            console.log(error)
+            toast("Something went wrong", {
+                description: new Date().toLocaleString(),
+                action: {
+                    label: "Hide",
+                    onClick: () => {}
+                }
+            })
+        }
+    }
+
   return (
     <div className="mt-10 flex flex-wrap gap-2.5" >
 
-        <Button className={buttonClass} >
+        <Button className={buttonClass} onClick={() => handleSignInFunction("github")} >
             <Image
                 src="/icons/github.svg"
                 alt="Github logo"
@@ -16,7 +40,7 @@ const SocialAuthForm = () => {
             />
             <span>Log in with GitHub</span>
         </Button>
-        <Button className={buttonClass} >
+        <Button className={buttonClass} onClick={() => handleSignInFunction("google")} >
             <Image
                 src="/icons/google.svg"
                 alt="Google logo"
--- my-app/tailwind.config.ts
+++ my-app/tailwind.config.ts
@@ -62,6 +62,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  // plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 };
 export default config;
